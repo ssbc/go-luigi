a107ca86a8ff308d1aacac6483f6998637ec99cb
keks <keks@cryptoscope.co>
2018-12-28 22:49:12 +0100

    better BufferedPipe test

--- pipe_test.go
+++ pipe_test.go
@@ -2,6 +2,7 @@ package luigi // import "go.cryptoscope.co/luigi"
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -10,26 +11,53 @@ import (
 
 func TestBufferedPipe(t *testing.T) {
 	r := require.New(t)
-	src, sink := NewPipe(WithBuffer(2))
 
-	ctx := context.Background()
-	err := sink.Pour(ctx, 1)
-	r.NoError(err, "pouring first value")
-	err = sink.Pour(ctx, 2)
-	r.NoError(err, "pouring second value")
-	err = sink.Close()
-	r.NoError(err, "closing sink")
+	mkTest := func(bufSize int) func(*testing.T) {
+		return func(t *testing.T) {
+			src, sink := NewPipe(WithBuffer(bufSize))
 
-	v, err := src.Next(ctx)
-	r.NoError(err, "getting first value")
-	r.Equal(v, 1, "expected 1")
+			ctx := context.Background()
 
-	v, err = src.Next(ctx)
-	r.NoError(err, "getting first value")
-	r.Equal(v, 2, "expected 1")
+			for i := 0; i < bufSize; i++ {
+				err := sink.Pour(ctx, i)
+				r.NoErrorf(err, "pouring %d", i)
+			}
 
-	_, err = src.Next(ctx)
-	r.Equal(EOS{}, errors.Cause(err), "expected end-of-stream")
+			err := sink.Close()
+			r.NoError(err, "closing")
+
+			for i := 0; i < bufSize; i++ {
+				v, err := src.Next(ctx)
+				r.NoErrorf(err, "getting what I expect to be %d", i)
+				r.Equalf(v, i, "unexpected value %v, got %v", v, i)
+			}
+
+			_, err = src.Next(ctx)
+			r.Equal(EOS{}, errors.Cause(err), "expected end-of-stream")
+		}
+	}
+
+	sizes := func() []int {
+		var (
+			acc int
+			out = make([]int, 16)
+		)
+
+		for i := range out {
+			out[i] = acc
+			if acc == 0 {
+				acc++
+			} else {
+				acc *= 2
+			}
+		}
+
+		return out
+	}()
+
+	for _, size := range sizes {
+		t.Run(fmt.Sprint(size), mkTest(size))
+	}
 }
 
 func TestCloseWithError(t *testing.T) {
