247f91c3457a3934aa109bb2475185465ebfc822
Henry <cryptix@riseup.net>
2019-01-11 09:58:21 +0100

    Add test to check that we can close a running pour

---
+++ chan_test.go
@@ -0,0 +1,63 @@
+package luigi
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+// verify we can abort a pipe while it is waiting to read/write it's data
+
+func TestCloseWhileNext(t *testing.T) {
+	ctx := context.TODO()
+	r := require.New(t)
+
+	errc := make(chan error)
+
+	src, sink := NewPipe()
+
+	go func() {
+		errc <- sink.Close()
+	}()
+
+	v, err := src.Next(ctx)
+	r.Equal(EOS{}, err, "should return end of stream")
+	r.Nil(v)
+
+	r.NoError(<-errc)
+}
+
+func TestCloseWhilePour(t *testing.T) {
+	ctx := context.TODO()
+	r := require.New(t)
+
+	closeErr := make(chan error)
+	nextErr := make(chan error)
+
+	src, sink := NewPipe()
+
+	go func() {
+		time.Sleep(25 * time.Millisecond) // just make sure Pour() started
+		closeErr <- sink.Close()
+		go func() {
+			v, err := src.Next(ctx)
+			if v != nil {
+				nextErr <- errors.Errorf("expected nil value but got %v", v)
+			}
+			nextErr <- errors.Wrap(err, "error from next")
+			close(nextErr)
+		}()
+	}()
+
+	err := sink.Pour(ctx, "test msg")
+	r.Equal(EOS{}, err, "should return end of stream")
+
+	r.NoError(<-closeErr)
+
+	for err := range nextErr {
+		r.NoError(err)
+	}
+}
